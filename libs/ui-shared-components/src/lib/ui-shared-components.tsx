7ce45795ca141bf176d7c031895345a598534d92
gilvaju <gcostajr@gmail.com>
2022-01-13 09:43:43 -0300

    feat: adicionando primeira lib react

---
+++ libs/ui-shared-components/src/lib/ui-shared-components.tsx
@@ -0,0 +1,14 @@
+import './ui-shared-components.module.css';
+
+/* eslint-disable-next-line */
+export interface UiSharedComponentsProps {}
+
+export function UiSharedComponents(props: UiSharedComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to UiSharedComponents!</h1>
+    </div>
+  );
+}
+
+export default UiSharedComponents;
