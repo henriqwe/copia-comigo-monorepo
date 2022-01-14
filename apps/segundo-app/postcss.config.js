08a63e13dc17b692d9c2be98a4b6e4dffa6cd690
gilvaju <gcostajr@gmail.com>
2022-01-14 10:03:53 -0300

    feat: aplicando tailwind para segundo-app

---
+++ apps/segundo-app/postcss.config.js
@@ -0,0 +1,11 @@
+// apps/primeiro-app/postcss.config.js
+const { join } = require('path');
+
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
---
+++ apps/segundo-app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    require('../../tailwind-workspace-preset.js')
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
