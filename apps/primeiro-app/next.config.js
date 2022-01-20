13263038271b551c56007beb513fa29e7c3ed053
henriqwe <henriqwe96@gmail.com>
2022-01-20 17:45:19 -0300

    update

--- apps/primeiro-app/next.config.js
+++ apps/primeiro-app/next.config.js
@@ -5,16 +5,23 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false
-  },
-  reactStrictMode: true,
+    nx: {
+        // Set this to true if you would like to to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: false
+    },
+    reactStrictMode: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    }
 };
 
 const plugins = [
-  [withNx],
+    [withNx],
 ];
 
 module.exports = withNx(nextConfig);
